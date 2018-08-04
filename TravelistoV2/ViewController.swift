39bfcfd65cff66fe8fabb8f118de723508cb652e
Chidi Emeh <chidiemeh184@gmail.com>
2018-08-04 18:29:53 -0400

    Initial Commit

---
+++ TravelistoV2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TravelistoV2
+//
+//  Created by Chidi Emeh on 8/4/18.
+//  Copyright © 2018 Chidi Emeh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
