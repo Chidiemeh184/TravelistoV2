4ef65ad9b621f4ec1afe7acf8f0f0c9fed6ec75c
Chidi Emeh <chidiemeh184@gmail.com>
2018-08-05 10:16:50 -0400

    Added second test

---
+++ TravelistoV2/test2.swift
@@ -0,0 +1,15 @@
+//
+//  test2.swift
+//  TravelistoV2
+//
+//  Created by Chidi Emeh on 8/5/18.
+//  Copyright © 2018 Chidi Emeh. All rights reserved.
+//
+
+import Foundation
+
+struct test2 {
+    
+    
+    
+}
